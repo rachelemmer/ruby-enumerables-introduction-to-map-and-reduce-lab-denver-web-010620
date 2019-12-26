805a9908c39aec69dd19e1ab099c0a92b6e40a9f
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-26 17:19:55 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,5 +38,13 @@ def map_to_square(source_array)
   return new_array
 end
 
-
+def reduce_to_total(source_array, starting_point = 0)
+  total = starting_point
+  counter = 0 
+  while counter < array.length do 
+    total += array[counter]
+    counter += 1 
+  end 
+  total
+end 
   
