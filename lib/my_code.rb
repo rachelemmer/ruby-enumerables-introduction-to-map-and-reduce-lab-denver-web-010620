ce192aff60487473608561a134440ce6f2f4892b
Rachel Emmer <rachelaemmer@gmail.com>
2019-12-26 17:17:45 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,41 +1,41 @@
 def map_to_negativize(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 ) 
-    i += 1
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array.push(source_array[counter] * -1 ) 
+    counter += 1
   end
-  return new
+  return new_array
 end
 
 def map_to_no_change(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] ) 
-    i += 1
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array.push(source_array[counter] * 1) 
+    counter += 1
   end
-  return new
+  return new_array
 end
 
 def map_to_double(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * 2 )
-    i += 1
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array.push(source_array[counter] * 2 )
+    counter += 1
   end
-  return new
+  return new_array
 end
 
 def map_to_square(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * source_array[i] ) 
-    i += 1
+  new_array = []
+  counter = 0
+  while counter < source_array.length do
+    new_array.push(source_array[counter] * source_array[counter]) 
+    counter += 1
   end
-  return new
+  return new_array
 end
 
 
